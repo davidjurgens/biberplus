025bf9cccc863b00af180a4ac28d74f04fec791b
kenana95 <kenan.r.alkiek@gmail.com>
2023-02-08 10:09:08 -0500

    Iniital commit

--- src/tagger/tagger_utils.py
+++ src/tagger/tagger_utils.py
@@ -1,3 +1,10 @@
+import stanza
+
+stanza.download('en')
+nlp = stanza.Pipeline('en')
+doc = nlp("Barack Obama was born in Hawaii.  He was elected president in 2008.")
+doc.sentences[0].print_dependencies()
+
 def read_in_variables(txt_file):
     variables = []
     with open(txt_file, 'r') as f:
