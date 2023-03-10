88ed659d2704bc2caefb34d57067ab77c91ca879
kenana95 <kenan.r.alkiek@gmail.com>
2023-03-09 19:43:30 -0500

    Run tagger function

--- src/tagger/tagger_main.py
+++ src/tagger/tagger_main.py
@@ -1,19 +1,25 @@
 import argparse
+import sys
 
+import pandas as pd
 import stanza
 
-from tagger_utils import build_variable_dictionaries, tagged_words_to_tsv
-from word_tagger import WordTagger
+sys.path.append('../..')
+
+from src.tagger.data_io import simple_text_batching
+from src.tagger.tagger_utils import build_variable_dictionaries, tagged_words_to_tsv
+from src.tagger.word_tagger import WordTagger
 
 
-def run_tagger(nlp_pipeline, text):
+def run_tagger(text, token_batch_size, use_gpu=True, show_progress=False):
+    pipeline = stanza.Pipeline(lang='en', processors='tokenize,pos', use_gpu=use_gpu)
     patterns_dict = build_variable_dictionaries()
-    doc = nlp_pipeline(text)
 
-    word_tagger = WordTagger(doc, patterns_dict)
-    word_tagger.run_all()
-
-    return word_tagger.tagged_words
+    for text_batch in simple_text_batching(text, token_batch_size, show_progress):
+        doc = pipeline(text_batch)
+        word_tagger = WordTagger(doc, patterns_dict)
+        word_tagger.run_all()
+        yield pd.DataFrame(word_tagger.tagged_words)
 
 
 def tag_text(text, use_gpu=True):
@@ -48,7 +54,6 @@ if __name__ == '__main__':
 
     parser.add_argument("--output_all_tags", default=False, type=str, action='store_true')
     parser.add_argument("--use_gpu", default=False, action='store_true', type=bool, required=False)
-
 
     args = parser.parse_args()
 
