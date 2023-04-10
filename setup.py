b8ca69742f56780e38c998ce30bf2d73829765fd
kenana95 <kenan.r.alkiek@gmail.com>
2023-04-10 15:33:37 -0400

    Setup initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='bibermda',
+    version='0.1.0',
+    description='A pure Python implementation of Biber\'s (1988) Variation across Speech and Writing linguistic tags',
+    url='https://github.com/davidjurgens/biber-multidimensional-register-analysis',
+    author='Kenan Alkiek, David Jurgens',
+    author_email='kalkiek@umich.edu',
+    license='MIT License',
+    packages=['pyexample'],
+    install_requires=['pandas', 'numpy', 'spacy', 'tqdm', 'en-core-web-sm'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
