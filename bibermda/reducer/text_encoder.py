5b6d84d132f9c36077d0b1a49442d4b67e12c947
kenana95 <kenan.r.alkiek@gmail.com>
2023-06-20 15:04:12 -0400

    10 signficant digits

--- bibermda/reducer/text_encoder.py
+++ bibermda/reducer/text_encoder.py
@@ -2,7 +2,7 @@ from bibermda.tagger import calculate_tag_frequencies
 from bibermda.tagger.constants import BIBER_PLUS_TAGS, DOC_TAGS
 
 
-def encode_text(config, text):
+def encode_text(config, text, round_to=10):
     frequencies_df = calculate_tag_frequencies(text, config=config)
     encodings = {}
 
@@ -12,13 +12,13 @@ def encode_text(config, text):
     # Split the counts by type
     if config['binary_tags']:
         binary_frequencies = frequencies_df[frequencies_df['tag'].isin(binary_tags)]
-        encodings['binary'] = binary_frequencies[['mean', 'std']].to_numpy().flatten().tolist()
+        encodings['binary'] = binary_frequencies[['mean', 'std']].to_numpy().flatten().round(round_to).tolist()
 
     if config['function_words']:
         fw_frequencies = frequencies_df[~frequencies_df['tag'].isin(biber_tags + binary_tags)]
-        encodings['function_words'] = fw_frequencies.drop('tag', axis=1).to_numpy().flatten().tolist()
+        encodings['function_words'] = fw_frequencies.drop('tag', axis=1).to_numpy().round(round_to).tolist()
 
     frequencies_df = frequencies_df[frequencies_df['tag'].isin(biber_tags)]
-    encodings['biber'] = frequencies_df.drop('tag', axis=1).to_numpy().flatten().tolist()
+    encodings['biber'] = frequencies_df.drop('tag', axis=1).to_numpy().flatten().round(round_to).tolist()
 
     return encodings
