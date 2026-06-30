89283dfe72f8c846776a904b8c4d2e3741c41f8c
Kenan Radwan Alkiek <kalkiek@umich.edu>
2026-06-30 16:39:43 +0000

    chore: read package version from installed metadata

--- biberplus/__init__.py
+++ biberplus/__init__.py
@@ -1,4 +1,10 @@
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("biberplus")
+except PackageNotFoundError:  # package not installed (e.g. running from source)
+    __version__ = "0.4.0"
+
 __author__ = 'Kenan Alkiek'
 __credits__ = 'University of Michigan - The Blablablab'
 
